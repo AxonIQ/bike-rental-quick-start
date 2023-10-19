a05517a81bbb38ce2d4f241af07502afcebc87bc
Ben Runchey <Ben Runchey>
2023-10-19 15:49:27 -0500

    Reverting back to before Java 21 Upgrade attempt

--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/PreparePaymentCommand.java
+++ core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/PreparePaymentCommand.java
@@ -1,6 +1,7 @@
 package io.axoniq.demo.bikerental.coreapi.payment;
 
 import org.axonframework.commandhandling.RoutingKey;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public record PreparePaymentCommand(int amount, @RoutingKey String paymentReference) {
 }
