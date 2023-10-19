d38aad32fe10223a427fe4dd110d8d054ae1a4f1
Ben Runchey <Ben Runchey>
2023-10-19 13:09:39 -0500

    Upgrade to Java 21 Remove unused classes and imports

--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/PreparePaymentCommand.java
+++ core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/PreparePaymentCommand.java
@@ -1,7 +1,6 @@
 package io.axoniq.demo.bikerental.coreapi.payment;
 
 import org.axonframework.commandhandling.RoutingKey;
-import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public record PreparePaymentCommand(int amount, @RoutingKey String paymentReference) {
 }
