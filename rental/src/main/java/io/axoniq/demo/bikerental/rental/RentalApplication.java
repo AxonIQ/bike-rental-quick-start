d5bc6d888828d700d484362def808d8d1ef096c7
Ben Runchey <Ben Runchey>
2024-04-22 13:54:30 -0500

    Including missed file!!

--- rental/src/main/java/io/axoniq/demo/bikerental/rental/RentalApplication.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/rental/RentalApplication.java
@@ -1,27 +1,29 @@
 package io.axoniq.demo.bikerental.rental;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.axoniq.demo.bikerental.coreapi.rental.BikeStatus;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.Configuration;
 import org.axonframework.config.ConfigurationScopeAwareProvider;
 import org.axonframework.config.ConfigurerModule;
 import org.axonframework.deadline.DeadlineManager;
 import org.axonframework.deadline.SimpleDeadlineManager;
-import org.axonframework.eventhandling.tokenstore.jpa.TokenEntry;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.eventhandling.saga.repository.MongoSagaStore;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.messaging.StreamableMessageSource;
-import org.axonframework.modelling.saga.repository.jpa.SagaEntry;
+import org.axonframework.modelling.saga.repository.SagaStore;
+import org.axonframework.serialization.Serializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-@EntityScan(basePackageClasses = {BikeStatus.class, SagaEntry.class, TokenEntry.class})
 @SpringBootApplication
 @EnableScheduling
 public class RentalApplication {
@@ -69,5 +71,31 @@ public class RentalApplication {
                         (c, b) -> b.workerExecutor(workerExecutorService())
                                    .batchSize(100)
                 );
+    }
+
+
+    @Bean
+    public DefaultMongoTemplate axonMongoTemplate(MongoTemplate mongoTemplate) {
+        return DefaultMongoTemplate.builder()
+                .mongoDatabase(mongoTemplate.getMongoDatabaseFactory().getMongoDatabase())
+                .build();
+    }
+
+    @Bean
+    public TokenStore mongoTokenStore(DefaultMongoTemplate axonMongoTemplate, Serializer serializer) {
+
+        return MongoTokenStore.builder()
+                .mongoTemplate(axonMongoTemplate)
+                .serializer(serializer)
+                .build();
+    }
+
+    @Bean
+    public SagaStore mongoSagaStore(DefaultMongoTemplate axonMongoTemplate, Serializer serializer) {
+
+        return MongoSagaStore.builder()
+                .mongoTemplate(axonMongoTemplate)
+                .serializer(serializer)
+                .build();
     }
 }
