365a7e9c23dcfda3ebda5616b6ddfdfa26662853
Mateusz Nowak <mateusz@nakodach.pl>
2025-01-02 13:00:15 +0100

    Revert "remove security"
    This reverts commit b09f6a32a0a852aedd23d7a907393df60ba1eccc.

---
+++ rental/src/main/java/io/axoniq/demo/bikerental/rental/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.axoniq.demo.bikerental.rental;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf().disable();
+        return http.build();
+    }
+}
